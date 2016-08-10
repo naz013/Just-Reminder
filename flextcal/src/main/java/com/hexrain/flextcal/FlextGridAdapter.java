154cd252e508c648d9ca0b67e5f83781800c82e5
Nazar <n.suhovich@gmail.com>
2016-08-10 18:08:19 +0300

    Replace gridview with recyclerView.

--- flextcal/src/main/java/com/hexrain/flextcal/FlextGridAdapter.java
+++ flextcal/src/main/java/com/hexrain/flextcal/FlextGridAdapter.java
@@ -3,10 +3,10 @@ package com.hexrain.flextcal;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -15,13 +15,7 @@ import java.util.HashMap;
 
 import hirondelle.date4j.DateTime;
 
-/**
- * The FlextGridAdapter provides customized view for the dates gridview
- * 
- * @author thomasdao
- * 
- */
-public class FlextGridAdapter extends BaseAdapter {
+public class FlextGridAdapter extends RecyclerView.Adapter<FlextGridAdapter.MyHolder> {
 	protected ArrayList<DateTime> datetimeList;
 	protected int month;
 	protected int year;
@@ -35,10 +29,10 @@ public class FlextGridAdapter extends BaseAdapter {
     protected int backgroundForToday = -1;
     protected HashMap<DateTime, Events> textMapForEvents;
 
-	/**
-	 * caldroidData belongs to Caldroid
-	 */
-	protected HashMap<String, Object> caldroidData;
+	protected HashMap<String, Object> mData;
+
+	protected OnItemClickListener onItemClickListener;
+	protected OnItemLongClickListener onItemLongClickListener;
 
 	public void setAdapterDateTime(DateTime dateTime) {
 		this.month = dateTime.getMonth();
@@ -46,49 +40,70 @@ public class FlextGridAdapter extends BaseAdapter {
 		this.datetimeList = FlextHelper.getFullWeeks(this.month, this.year, startDayOfWeek);
 	}
 
-	// GETTERS AND SETTERS
 	public ArrayList<DateTime> getDatetimeList() {
 		return datetimeList;
 	}
 
 	public void setCaldroidData(HashMap<String, Object> caldroidData) {
-		this.caldroidData = caldroidData;
-
-		// Reset parameters
+		this.mData = caldroidData;
 		populateFromCaldroidData();
 	}
 
-	/**
-	 * Constructor
-	 * 
-	 * @param context
-	 * @param month
-	 * @param year
-	 * @param caldroidData
-	 */
 	public FlextGridAdapter(Context context, int month, int year,
 							HashMap<String, Object> caldroidData, boolean isDark) {
 		super();
 		this.month = month;
 		this.year = year;
 		this.context = context;
-		this.caldroidData = caldroidData;
+		this.mData = caldroidData;
 		this.resources = context.getResources();
         this.isDark = isDark;
-
 		populateFromCaldroidData();
 	}
 
-	/**
-	 * Retrieve internal parameters from caldroid data
-	 */
-	@SuppressWarnings("unchecked")
-	private void populateFromCaldroidData() {
-		startDayOfWeek = (Integer) caldroidData.get(FlextCal.START_DAY_OF_WEEK);
-        backgroundForToday = (Integer) caldroidData.get(FlextCal._BACKGROUND_FOR_TODAY_);
-        textMapForEvents = (HashMap<DateTime, Events>) caldroidData
-                .get(FlextCal._EVENTS_);
+	public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+		this.onItemClickListener = onItemClickListener;
+	}
 
+	public void setOnItemLongClickListener(OnItemLongClickListener onItemLongClickListener) {
+		this.onItemLongClickListener = onItemLongClickListener;
+	}
+
+	class MyHolder extends RecyclerView.ViewHolder {
+
+		TextView cellView;
+		TextView task1;
+		TextView task2;
+
+		public MyHolder(View itemView) {
+			super(itemView);
+			cellView = (TextView) itemView.findViewById(R.id.textView);
+			task1 = (TextView) itemView.findViewById(R.id.task1);
+			task2 = (TextView) itemView.findViewById(R.id.task2);
+			cellView.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View view) {
+					if (onItemClickListener != null) {
+						onItemClickListener.onItemClick(view, getAdapterPosition());
+					}
+				}
+			});
+			cellView.setOnLongClickListener(new View.OnLongClickListener() {
+				@Override
+				public boolean onLongClick(View view) {
+					if (onItemLongClickListener != null) {
+						onItemLongClickListener.onItemLongClick(view, getAdapterPosition());
+					}
+					return false;
+				}
+			});
+		}
+	}
+
+	private void populateFromCaldroidData() {
+		startDayOfWeek = (Integer) mData.get(FlextCal.START_DAY_OF_WEEK);
+        backgroundForToday = (Integer) mData.get(FlextCal._BACKGROUND_FOR_TODAY_);
+        textMapForEvents = (HashMap<DateTime, Events>) mData.get(FlextCal._EVENTS_);
 		this.datetimeList = FlextHelper.getFullWeeks(this.month, this.year, startDayOfWeek);
 	}
 
@@ -101,7 +116,6 @@ public class FlextGridAdapter extends BaseAdapter {
 
 	protected void setCustomResources(DateTime dateTime, TextView task1, TextView task2) {
 		if (textMapForEvents != null) {
-			// Set it
 			if (textMapForEvents.containsKey(dateTime)) {
 				Events events = textMapForEvents.get(dateTime);
 				if (events.hasNext()) {
@@ -119,30 +133,17 @@ public class FlextGridAdapter extends BaseAdapter {
 		}
 	}
 
-	/**
-	 * Customize colors of text and background based on states of the cell
-	 * (disabled, active, selected, etc)
-	 * 
-	 * To be used only in getView method
-     * @param position
-     * @param cellView
-     * @param task1
-     * @param task2
-     */
 	protected void customizeTextView(int position, TextView cellView, TextView task1, TextView task2) {
         if (isDark){
             cellView.setTextColor(resources.getColor(android.R.color.white));
         } else {
             cellView.setTextColor(resources.getColor(android.R.color.black));
         }
-
 		DateTime dateTime = this.datetimeList.get(position);
-
 		boolean notCurrent = dateTime.getMonth() != month;
 		if (notCurrent) {
 			cellView.setTextColor(resources.getColor(android.R.color.darker_gray));
 		}
-
 		if (dateTime.equals(getToday())) {
 			if (backgroundForToday != 0) {
 				cellView.setTextColor(backgroundForToday);
@@ -152,41 +153,36 @@ public class FlextGridAdapter extends BaseAdapter {
 		} else {
 			cellView.setBackgroundResource(android.R.color.transparent);
 		}
-
 		cellView.setText(String.valueOf(dateTime.getDay()));
-
 		if (!notCurrent) setCustomResources(dateTime, task1, task2);
 	}
 
 	@Override
-	public int getCount() {
-		// TODO Auto-generated method stub
-		return this.datetimeList.size();
+	public MyHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+		LayoutInflater inflater = LayoutInflater.from(context);
+		return new MyHolder(inflater.inflate(R.layout.date_cell, null, false));
 	}
 
 	@Override
-	public Object getItem(int arg0) {
-		// TODO Auto-generated method stub
-		return null;
+	public void onBindViewHolder(MyHolder holder, int position) {
+		customizeTextView(position, holder.cellView, holder.task1, holder.task2);
 	}
 
 	@Override
 	public long getItemId(int arg0) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-		LayoutInflater inflater = (LayoutInflater) context
-				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	public int getItemCount() {
+		return datetimeList.size();
+	}
 
-		View view = inflater.inflate(R.layout.date_cell, null, false);
+	public interface OnItemClickListener {
+		void onItemClick(View view, int position);
+	}
 
-        TextView cellView = (TextView) view.findViewById(R.id.textView);
-        TextView task1 = (TextView) view.findViewById(R.id.task1);
-        TextView task2 = (TextView) view.findViewById(R.id.task2);
-		customizeTextView(position, cellView, task1, task2);
-		return view;
+	public interface OnItemLongClickListener {
+		void onItemLongClick(View view, int position);
 	}
 }
